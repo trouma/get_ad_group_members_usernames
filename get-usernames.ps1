bd7a45bce530e17366523e60eddec91c8e1c63a9
trouma <trouma@gmail.com>
2019-08-26 10:44:19 +0200

    Add files via upload

---
+++ get-usernames.ps1
@@ -0,0 +1,3 @@
+﻿$Groupname = Read-Host -Prompt "Please enter the group name: "
+Get-ADGroupMember -Identity $Groupname  | ft -Property SamAccountName > $env:TEMP\$groupname.txt
+start $env:windir\system32\notepad.exe $env:TEMP\$groupname.txt
